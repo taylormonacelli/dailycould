ba32c0a37d1d34ba8002ef5af3bb5c8b91a7128e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-07 15:42:56 -0700

    refactor version fetching/printing

--- {{ cookiecutter.project_slug }}/cmd/version.go
+++ {{ cookiecutter.project_slug }}/cmd/version.go
@@ -18,9 +18,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Version: %s, %s\n", version.Version, version.FullGitSHA)
-		fmt.Printf("Build Date: %s\n", version.Date)
-		fmt.Printf("Go Version: %s\n", version.GoVersion)
+		buildInfo := version.GetBuildInfo()
+		fmt.Println(buildInfo)
 	},
 }
 
