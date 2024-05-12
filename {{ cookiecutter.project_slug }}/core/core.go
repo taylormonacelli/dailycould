076791bdc6e83dddfabd2fe6eeaa3a1ddd0fbe58
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-12 10:01:23 -0700

    Add api example

--- {{ cookiecutter.project_slug }}/core/core.go
+++ {{ cookiecutter.project_slug }}/core/core.go
@@ -1,35 +1,7 @@
-package version
+package core
 
 import "fmt"
 
-type BuildInfo struct {
-	Date        string
-	FullGitSHA  string
-	GoVersion   string
-	ShortGitSHA string
-	Version     string
-}
-
-func (bi BuildInfo) String() string {
-	return fmt.Sprintf(`Version: %s, %s
-Build Date: %s
-Go Version: %s`, bi.Version, bi.FullGitSHA, bi.Date, bi.GoVersion)
-}
-
-var (
-	Date        string
-	FullGitSHA  string
-	GoVersion   string
-	ShortGitSHA string
-	Version     string
-)
-
-func GetBuildInfo() BuildInfo {
-	return BuildInfo{
-		Date:        Date,
-		FullGitSHA:  FullGitSHA,
-		GoVersion:   GoVersion,
-		ShortGitSHA: ShortGitSHA,
-		Version:     Version,
-	}
+func Hello() {
+	fmt.Println("Hello, World!")
 }
